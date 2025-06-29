ffee3c74cb056230d24dd26dcb6d73d55f746379
Kobayashi24730 <ajenteduplo000@gmail.com>
2025-06-29 20:00:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 import os  
 
 app = Flask(__name__)  
-CORS(app, resources={r"/*": {"origins": "*"}})  
+CORS(app, resources={r"/*": {"origins": "https://kobayashi24730.github.io"}})
 
 database_url = os.environ.get("DATABASE_URL")
 if database_url and database_url.startswith("postgres://"):
