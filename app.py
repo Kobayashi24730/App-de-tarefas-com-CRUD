46365b323eaf2f558bfc0865f804f7859025babd
Kobayashi24730 <ajenteduplo000@gmail.com>
2025-06-29 18:46:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 import os  
 
 app = Flask(__name__)  
-CORS(app)  
+CORS(app, resources={r"/*": {"origins": "*"}})  
 
 database_url = os.environ.get("DATABASE_URL")  
 if not database_url:  
