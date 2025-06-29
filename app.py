4cfb4b6981646c321c82812447e21e0250a8d91d
Kobayashi24730 <ajenteduplo000@gmail.com>
2025-06-29 19:06:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,14 @@ import os
 app = Flask(__name__)  
 CORS(app, resources={r"/*": {"origins": "*"}})  
 
-database_url = os.environ.get("DATABASE_URL")  
-if not database_url:  
-    database_url = "sqlite:///tasks.db"   
+database_url = os.environ.get("DATABASE_URL")
+if database_url and database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = database_url  
+if not database_url:
+    database_url = "sqlite:///tasks.db"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = database_url
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False  
 app.config["JWT_SECRET_KEY"] = "991652"  
 
